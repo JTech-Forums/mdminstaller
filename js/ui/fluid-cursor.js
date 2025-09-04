9efee6e67eda1442a3d8d5c57b32199fcf0ccc4e
TripleU <tripleuworld@gmail.com>
2025-09-04 13:23:39 -0400

    feat: add trailing fluid cursor

--- js/ui/fluid-cursor.js
+++ js/ui/fluid-cursor.js
@@ -1,24 +1,52 @@
 class FluidCursor {
     constructor() {
-        this.cursor = document.createElement('div');
-        this.cursor.className = 'fluid-cursor';
-        document.body.appendChild(this.cursor);
         document.body.classList.add('hide-cursor');
+
+        // SVG filter for gooey effect
+        document.body.insertAdjacentHTML('afterbegin',
+            '<svg xmlns="http://www.w3.org/2000/svg" width="0" height="0"><defs><filter id="gooey"><feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur"/><feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 20 -10" result="goo"/><feComposite in="SourceGraphic" in2="goo" operator="atop"/></filter></defs></svg>'
+        );
+
+        this.container = document.createElement('div');
+        this.container.className = 'fluid-cursor';
+
+        this.dot = document.createElement('div');
+        this.dot.className = 'cursor-dot';
+        this.follow = document.createElement('div');
+        this.follow.className = 'cursor-follow';
+        this.trail = document.createElement('div');
+        this.trail.className = 'cursor-trail';
+
+        this.container.appendChild(this.trail);
+        this.container.appendChild(this.follow);
+        this.container.appendChild(this.dot);
+        document.body.appendChild(this.container);
+
         this.pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
-        this.mouse = { x: this.pos.x, y: this.pos.y };
-        this.speed = 0.25;
+        this.followPos = { ...this.pos };
+        this.trailPos = { ...this.pos };
+
         document.addEventListener('mousemove', (e) => {
-            this.mouse.x = e.clientX;
-            this.mouse.y = e.clientY;
+            this.pos.x = e.clientX;
+            this.pos.y = e.clientY;
+            this.dot.style.transform = `translate3d(${this.pos.x}px, ${this.pos.y}px,0)`;
         });
+
         this.render = this.render.bind(this);
         requestAnimationFrame(this.render);
     }
 
     render() {
-        this.pos.x += (this.mouse.x - this.pos.x) * this.speed;
-        this.pos.y += (this.mouse.y - this.pos.y) * this.speed;
-        this.cursor.style.transform = `translate3d(${this.pos.x}px, ${this.pos.y}px, 0)`;
+        // follower slightly lags behind cursor
+        this.followPos.x += (this.pos.x - this.followPos.x) * 0.2;
+        this.followPos.y += (this.pos.y - this.followPos.y) * 0.2;
+        this.follow.style.transform = `translate3d(${this.followPos.x}px, ${this.followPos.y}px,0)`;
+
+        // trailing blob lags further creating fluid tail
+        this.trailPos.x += (this.followPos.x - this.trailPos.x) * 0.25;
+        this.trailPos.y += (this.followPos.y - this.trailPos.y) * 0.25;
+        this.trail.style.transform = `translate3d(${this.trailPos.x}px, ${this.trailPos.y}px,0)`;
+
         requestAnimationFrame(this.render);
     }
 }
@@ -36,3 +64,4 @@ if (typeof module !== 'undefined' && module.exports) {
         init();
     }
 }
+
